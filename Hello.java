100aaba41ae6e479e5f4512d927c52eaf8464662
JinHo Bae <pookgoon@naver.com>
2018-01-30 11:46:09 +0900

    added output "tell her world"

--- Hello.java
+++ Hello.java
@@ -3,6 +3,8 @@ public class HelloGit{
 	public static void main(String[]args){
 		System.out.println("hello git");
 		system.out.println("tell your world");
+		system.out.println("tell her world"):
 	}
 }
+
 
